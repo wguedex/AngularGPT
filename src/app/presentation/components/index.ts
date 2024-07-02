9534e15204a5a686175c87b07f2130d9e299c820
Wilmer Guedez <w.guedex@gmail.com>
2024-07-02 17:14:17 -0500

    angular-gpt: alias de componentes

---
+++ src/app/presentation/components/index.ts
@@ -0,0 +1,4 @@
+export { SidebarMenuItemComponent } from "./sidebarMenuItem/sidebarMenuItem.component"; 
+export { ChatMessageComponent } from "./chat-bubbles/chatMessage/chatMessage.component";
+export { MyMessageComponent } from "./chat-bubbles/myMessage/myMessage.component";
+ 
--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ChatMessageComponent } from '../../components/chat-bubbles/chatMessage/chatMessage.component';
-import { MyMessageComponent } from '../../components/chat-bubbles/myMessage/myMessage.component';
+import { ChangeDetectionStrategy, Component } from '@angular/core'; 
+import { ChatMessageComponent, MyMessageComponent } from '@components/index'; 
 
 @Component({
     selector: 'app-orthography-page',
