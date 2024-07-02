110f138814d63631f4b2967a99ad2ba58a99edc7
Wilmer Guedez <w.guedex@gmail.com>
2024-07-02 18:08:54 -0500

    angular-gpt: comunicacion entre componentes

--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -15,4 +15,10 @@ import { ChatMessageComponent, MyMessageComponent, TextMessageBoxComponent, Typi
     templateUrl: './orthographyPage.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class OrthographyPageComponent { }
+export default class OrthographyPageComponent { 
+
+    handleMessage(prompt: string) {
+        
+    }
+
+}
